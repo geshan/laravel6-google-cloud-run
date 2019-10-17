8d35117ac306f7dc0985cc8afea0712a511f2250
Geshan Manandhar <geshan.manandhar@theiconic.com.au>
2019-10-17 11:56:04 +1100

    Adding logging with logentries

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,17 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Log\LogManager;
 
 class HomeController extends Controller
 {
+    private $logger;
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(LogManager $logger)
     {
-        $this->middleware('auth');
+        $this->logger = $logger;
+        //$this->middleware('auth');
     }
 
     /**
@@ -23,6 +26,22 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $this->logger->info('test', ['var' => 'val']);
+        $this->logger->channel('logentries')->info('testing', ['type' => 1]);
+
+        try {
+            throw new \Exception('this in an Exception');
+        } catch (\Exception $e) {
+            $this->logger->channel('logentries')->error(
+                sprintf('This is an error - %s', $e->getMessage()),
+                [
+                    'file' => $e->getFile(),
+                    'lineNo' => $e->getLine()
+                ]
+            );
+        }
+
+
         return view('home');
     }
 }
